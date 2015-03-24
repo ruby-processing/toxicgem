4cf4b5e5c4aa37d2cc9d7b13deed26385761547a
monkstone <martin_p@lineone.net>
2015-03-24 10:14:23 +0000

    modified:   example/gray_scott_tone_map.rb 	modified:   ext/toxi/color/AlphaAccessor.java 	modified:   ext/toxi/color/CMYKAccessor.java 	modified:   ext/toxi/color/ColorGradient.java 	modified:   ext/toxi/color/ColorList.java 	modified:   ext/toxi/color/ColorRange.java 	modified:   ext/toxi/color/ColorTheme.java 	modified:   ext/toxi/color/HSVAccessor.java 	modified:   ext/toxi/color/HSVDistanceProxy.java 	modified:   ext/toxi/color/HistEntry.java 	modified:   ext/toxi/color/Histogram.java 	modified:   ext/toxi/color/Hue.java         modified:   ext/toxi/color/LuminanceAccessor.java 	modified:   ext/toxi/color/ProximityComparator.java 	modified:   ext/toxi/color/RGBAccessor.java 	modified:   ext/toxi/color/RGBDistanceProxy.java 	modified:   ext/toxi/color/ReadonlyTColor.java 	modified:   ext/toxi/color/theory/AnalogousStrategy.java 	modified:   ext/toxi/color/theory/ColorTheoryRegistry.java 	modified:   ext/toxi/color/theory/ComplementaryStrategy.java 	modified:   ext/toxi/color/theory/CompoundTheoryStrategy.java 	modified:   ext/toxi/color/theory/LeftSplitComplementaryStrategy.java 	modified:   ext/toxi/color/theory/MonochromeTheoryStrategy.java 	modified:   ext/toxi/color/theory/RightSplitComplementaryStrategy.java 	modified:   ext/toxi/color/theory/SingleComplementStrategy.java 	modified:   ext/toxi/color/theory/SplitComplementaryStrategy.java 	modified:   ext/toxi/color/theory/TetradTheoryStrategy.java 	modified:   ext/toxi/color/theory/TriadTheoryStrategy.java 	modified:   ext/toxi/sim/automata/CAMatrix.java 	modified:   ext/toxi/sim/automata/CARule2D.java 	modified:   ext/toxi/sim/automata/CAWolfram1D.java 	modified:   ext/toxi/sim/dla/BottomUpOrder.java 	modified:   ext/toxi/sim/dla/DLA.java 	modified:   ext/toxi/sim/dla/DLAEventAdapter.java 	modified:   ext/toxi/sim/dla/DLAGuideLines.java 	modified:   ext/toxi/sim/dla/DLAParticle.java 	modified:   ext/toxi/sim/dla/DLASegment.java 	modified:   ext/toxi/sim/dla/PipelineOrder.java 	modified:   ext/toxi/sim/dla/RadialDistanceOrder.java 	modified:   ext/toxi/sim/erosion/ErosionFunction.java 	modified:   ext/toxi/sim/erosion/TalusAngleErosion.java 	modified:   ext/toxi/sim/erosion/ThermalErosion.java 	example/inflate_mesh.rb 	ext/toxi/data/ 	ext/toxi/image/ 	ext/toxi/newmesh/ 	ext/toxi/physics2d/ 	ext/toxi/physics3d/ 	ext/toxi/processing/ 	ext/toxi/volume/

--- example/gray_scott_tone_map.rb
+++ example/gray_scott_tone_map.rb
@@ -2,7 +2,7 @@ require 'jruby_art'
 require 'toxiclibs'
 
 class GrayScottToneMap < Processing::App 
-  include Toxi
+  include Simulation
 
   #
   # <p>GrayScottToneMap shows how to use the ColorGradient & ToneMap classes of the
@@ -43,23 +43,23 @@ class GrayScottToneMap < Processing::App
   
   def setup 
       size(256,256)
-      @gs= Toxi::GrayScott.new width,height, false
+      @gs= Simulation::GrayScott.new width,height, false
       @gs.set_coefficients 0.021, 0.076, 0.12, 0.06
       # create a color gradient for 256 values
-      grad = Toxi::ColorGradient.new
+      grad = Simulation::ColorGradient.new
       # NamedColors are preset colors, but any TColor can be added
       # see javadocs for list of names:
       # http://toxiclibs.org/docs/colorutils/toxi/color/NamedColor::html
       # NB: use '::' in place of '.' here for these java constants
-      grad.add_color_at(0, Toxi::NamedColor::BLACK)
-      grad.add_color_at(128, Toxi::NamedColor::RED)
-      grad.add_color_at(192, Toxi::NamedColor::YELLOW)
-      grad.add_color_at(255, Toxi::NamedColor::WHITE)
+      grad.add_color_at(0, Simulation::NamedColor::BLACK)
+      grad.add_color_at(128, Simulation::NamedColor::RED)
+      grad.add_color_at(192, Simulation::NamedColor::YELLOW)
+      grad.add_color_at(255, Simulation::NamedColor::WHITE)
       # this gradient is used to map simulation values to colors
       # the first 2 parameters define the min/max values of the
       # input range (Gray-Scott produces values in the interval of 0.0 - 0.5)
       # setting the max = 0.33 increases the contrast
-      @tone_map = Toxi::ToneMap.new 0, 0.33, grad
+      @tone_map = Simulation::ToneMap.new 0, 0.33, grad
   end
   
   def draw     
--- lib/toxiclibs.rb
+++ lib/toxiclibs.rb
@@ -1,7 +1,22 @@
 require 'toxiclibs.jar'
 
-module Toxi
+module Simulation
   include_package 'toxi.sim.grayscott'
   include_package 'toxi.math'
   include_package 'toxi.color'
+end
+
+module Physics
+  include_package 'toxi.physics2d.behaviors'
+  include_package 'toxi.physics2d'
+  include_package 'toxi.geom'
+  include_package 'toxi.math'
+end
+
+module Volume
+  include_package 'toxi.volume'
+end
+
+module Gfx
+  include_package 'toxi.processing'
 end
