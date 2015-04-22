1dbccc1b5db999e4e98cf2ff8587ac8278af101b
monkstone <martin_p@lineone.net>
2015-04-22 18:15:21 +0100

    favour java import over package include

--- lib/toxiclibs.rb
+++ lib/toxiclibs.rb
@@ -1,51 +1,83 @@
-if RUBY_PLATFORM == 'java'  
+if RUBY_PLATFORM == 'java'
   require 'toxiclibs.jar'
-  
+
   module Simulation
     include_package 'toxi.sim.automata'
     include_package 'toxi.sim.dla'
-    include_package 'toxi.sim.erosion'
-    include_package 'toxi.sim.fluids'
-    include_package 'toxi.sim.grayscott'
+    erosion = %w(ErosionFunction TalusAngleErosion ThermalErosion)
+    erosion.each { |klass| java_import format('toxi.sim.erosion.%s', klass) }
+    fluids = %w(FluidSolver2D FluidSolver3D)
+    fluids.each { |klass| java_import format('toxi.sim.fluids.%s', klass) }
+    java_import 'toxi.sim.grayscott.GrayScott'
   end
-  
+
   module Physics
     behavior2d = %w(AttractionBehavior2D ConstantForceBehavior2D GravityBehavior2D ParticleBehavior2D)
-    behavior2d.each{ |klass| java_import format('toxi.physics2d.behaviors.%s', klass) }
+    behavior2d.each { |klass| java_import format('toxi.physics2d.behaviors.%s', klass) }
     include_package 'toxi.physics2d.constraints'
     include_package 'toxi.physics2d'
     behavior3d = %w(AttractionBehavior3D ConstantForceBehavior3D GravityBehavior3D ParticleBehavior3D)
-    behavior3d.each{ |klass| java_import format('toxi.physics3d.behaviors.%s', klass) }
+    behavior3d.each { |klass| java_import format('toxi.physics3d.behaviors.%s', klass) }
     include_package 'toxi.physics3d.constraints'
     include_package 'toxi.physics3d'
   end
-  
+
   module Volume
     include_package 'toxi.volume'
   end
-  
+
   module Toxi
     include_package 'toxi.color'
     include_package 'toxi.color.theory'
     include_package 'toxi.util.datatypes'
     java_import 'toxi.util.events.EventDispatcher'
     utils = %w{DateUtils FileSequenceDescriptor FileUtils}
-    utils.each{ |klass| java_import format('toxi.util.%s', klass) }
-    include_package 'toxi.geom.mesh.subdiv'
-    include_package 'toxi.geom.nurbs'
-    include_package 'toxi.math' 
-    geom = %w(AABB Axis3D Circle Cone Vec2D Vec3D Vec4D Rect Shape2D Shape3D Sphere )
-    geom.each{ |klass| java_import format('toxi.geom.%s', klass) }
+    utils.each { |klass| java_import format('toxi.util.%s', klass) }
+    subdiv = %w(CentroidSubdiv DisplacementSubdivision DualDisplacementSubdivision
+                DualSubdivision EdgeLengthComparator FaceCountComparator
+                MidpointDisplacementSubdivision MidpointSubdiv MidpointSubdivision
+                NewSubdivStrategy NormalDisplacementSubdivision SubdivisionStrategy
+                TriSubdivision)
+    subdiv.each { |klass| java_import format('toxi.geom.mesh.subdiv.%s', klass) }
+    nurbs = %w(BasicNurbsCurve BasicNurbsSurface ControlNet CurveCreator CurveUtils
+               InterpolationException KnotVector NurbsCreator NurbsCurve NurbsMeshCreator
+               NurbsSurface)
+    nurbs.each { |klass| java_import format('toxi.geom.nurbs.%s', klass) }
+    include_package 'toxi.math'
+    geom = %w(AABB Axis3D AxisAlignedCylinder BernsteinPolynomial BezierCurve2D
+              BezierCurve3D BooleanShapeBuilder BoxIntersector Circle CircleIntersector
+              Cone ConvexPolygonClipper CoordinateExtractor Ellipse GMatrix GVector
+              GlobalGridTesselator GridTesselator Intersector2D Intersector3D IsectData2D
+              IsectData3D Line2D Line3D LineStrip2D LineStrip3D LocalGridTesselator Matrix3d
+              Matrix4f Matrix4x4 MatrixSizeException OctreeVisitor Origin3D Plane
+              PlaneIntersector PointCloud3D PointOctree PointQuadtree Polygon2D
+              PolygonClipper2D PolygonTesselator QuadtreeVisitor Quaternion Ray2D Ray3D
+              Ray3DIntersector ReadonlyVec2D ReadonlyVec3D ReadonlyVec4D Rect Reflector3D
+              Shape2D Shape3D SingularMatrixException SpatialBins SpatialIndex Sphere
+              SphereIntersectorReflector Spline2D Spline3D SutherlandHodgemanClipper
+              Triangle2D Triangle3D TriangleIntersector Vec2D Vec3D Vec4D VecMathUtil
+              XAxisCylinder YAxisCylinder ZAxisCylinder)
+    geom.each { |klass| java_import format('toxi.geom.%s', klass) }
   end
-  
+
   module Gfx
     include_package 'toxi.processing'
   end
-  
-  # to disambiguate our own Vec2D and Vec3D we give new name to toxi versions  
+
+  # to disambiguate our own Vec2D and Vec3D we give new name to toxi versions
   TVec2D = Toxi::Vec2D
   TVec3D = Toxi::Vec3D
   AABB = Toxi::AABB
-  mesh = %w(Mesh3D TriangleMesh Face WEFace WETriangleMesh Vertex WEVertex LaplacianSmooth)
-  mesh.each{ |klass| java_import format('toxi.geom.mesh.%s', klass) }
+  volume = %w(AdditiveBrush ArrayIsoSurface BoxBrush BrushMode HashIsoSurface
+              IsoSurface MarchingCubesIndex MeshLatticeBuilder MeshVoxelizer
+              MultiplyBrush PeakBrush ReplaceBrush RoundBrush VolumetricBrush
+              VolumetricHashMap VolumetricSpace VolumetricSpaceArray)
+  volume.each { |klass| java_import format('toxi.volume.%s', klass) }
+  mesh = %w(BezierPatch BoxSelector DefaultSTLColorModel DefaultSelector Face
+            LaplacianSmooth MaterialiseSTLColorModel Mesh3D MeshIntersector OBJWriter
+            PLYWriter PlaneSelector STLColorModel STLReader STLWriter SphereFunction
+            SphericalHarmonics SuperEllipsoid SurfaceFunction SurfaceMeshBuilder
+            Terrain TriangleMesh Vertex VertexSelector WEFace WEMeshFilterStrategy
+            WETriangleMesh WEVertex WingedEdge)
+  mesh.each { |klass| java_import format('toxi.geom.mesh.%s', klass) }
 end
