b324d6bb574fe277ed70a5b6b9d9cf3d67d69f17
monkstone <mamba2928@yahoo.co.uk>
2017-01-07 08:22:36 +0000

    String.format

--- src/toxi/geom/Matrix4x4.java
+++ src/toxi/geom/Matrix4x4.java
@@ -992,16 +992,16 @@ public class Matrix4x4 {
 
      @Override
      public String toString() {
-       return String.join("|\n",
-       String.format("| %d %d %d %d ",
+       return String.join("\n",
+       String.format("| %d %d %d %d |",
        matrix[0][0], matrix[0][1], matrix[0][2], matrix[0][3]),
-       String.format("| %d %d %d %d ",
+       String.format("| %d %d %d %d |",
        matrix[1][0], matrix[1][1], matrix[1][2], matrix[1][3]),
-       String.format("| %d %d %d %d ",
+       String.format("| %d %d %d %d |",
        matrix[2][0], matrix[2][1],  matrix[2][2], matrix[2][3]),
-       String.format("| %d %d %d %d ",
-       matrix[3][0], matrix[3][1], matrix[3][2], matrix[3][3]))
-       + " |";
+       String.format("| %d %d %d %d |",
+       matrix[3][0], matrix[3][1], matrix[3][2], matrix[3][3])
+       );
      }
 
     /**
