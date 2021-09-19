70ffea7de981fc90db5ac16ed256cf8928c51bdb
monkstone <mamba2928@yahoo.co.uk>
2021-09-19 15:06:34 +0100

    remove com

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,16 +1,21 @@
 module toxiclibs{
   exports toxi.color;
-  exports toxi.data;
+  exports toxi.data.csv;
+  exports toxi.data.feeds;
   exports toxi.geom;
-  exports toxi.music;
+  exports toxi.image.util;
+  exports toxi.music.scale;
   exports toxi.newmesh;
   exports toxi.physics2d;
   exports toxi.physics3d;
   exports toxi.processing;
-  exports toxi.sim;
-  exports toxi.utils;
+  exports toxi.sim.automata;
+  exports toxi.sim.dla;
+  exports toxi.sim.erosion;
+  exports toxi.sim.fluids;
+  exports toxi.sim.grayscott;
+  exports toxi.util;
   exports toxi.volume;
-  exports com.toxi;
   requires org.processing.core;
   requires java.desktop;
   requires java.logging;
